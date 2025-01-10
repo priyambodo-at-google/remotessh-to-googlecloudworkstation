fedb89075a05c8281fdaa454df028df17a6ec35b
Doddi Priyambodo <doddi@bicarait.com>
2025-01-10 19:11:23 +0700

    Update remote SSH and tunnel scripts with specific project configurations and add git push script for user setup

--- 01.remote-ssh.sh
+++ 01.remote-ssh.sh
@@ -1,8 +1,15 @@
+# gcloud workstations ssh \
+#     --project=PROJECT_ID \
+#     --region=REGION \
+#     --cluster=CLUSTER_NAME \
+#     --config=CONFIG_NAME  \
+#     --port=WORKSTATION_PORT  \
+#     --local-host-port=localhost:LOCAL_PORT  \
+#     WORKSTATION_NAME
+
 gcloud workstations ssh \
-    --project=PROJECT_ID \
-    --region=REGION \
-    --cluster=CLUSTER_NAME \
-    --config=CONFIG_NAME  \
-    --port=WORKSTATION_PORT  \
-    --local-host-port=localhost:LOCAL_PORT  \
-    WORKSTATION_NAME
+  --project=work-mylab-machinelearning \
+  --cluster=cluster-priyambodo-workstation \
+  --config=config-priyambodo-workstation \
+  --region=us-central1 \
+  vscode-priyambodo-workstation
--- 02.remote-tunnel.sh
+++ 02.remote-tunnel.sh
@@ -1,8 +1,18 @@
+#run this command from local terminal
 gcloud workstations start-tcp-tunnel \
-    --project=PROJECT_ID \
-    --region=REGION \
-    --cluster=CLUSTER_NAME \
-    --config=CONFIG_NAME \
-    --local-host-port=localhost:LOCAL_PORT \
-    WORKSTATION_NAME \
-    WORKSTATION_PORT
+  --project=work-mylab-machinelearning \
+  --region=us-central1 \
+  --cluster=cluster-priyambodo-workstation \
+  --config=config-priyambodo-workstation \
+  vscode-priyambodo-workstation 22
+
+#run this command from local machine vscode/cursor
+#https://cloud.google.com/workstations/docs/develop-code-using-local-vscode-editor
+
+#Open or return to the VS Code application.
+#To launch Remote - SSH, click  Open a Remote Window in the status bar.
+Select the Connect to Host option when the command palette opens.
+Enter user@localhost:LOCAL_PORT, replacing LOCAL_PORT with your local port number.
+The following example connects to local port 1025 by specifying user@localhost:1025.
+user@localhost:55322
+#ssh -p 1025 user@localhost
